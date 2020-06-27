dd7db1d6699f9c7b623f27ba2160fa10d7ed61ab
Karlis Cars <karlis.cars@gmail.com>
2020-06-27 10:13:00 +0300

    Golden ratio app

---
+++ Golden ratio/goldenRatioViewViewController.swift
@@ -0,0 +1,42 @@
+//
+//  goldenRatioViewViewController.swift
+//  Golden ratio
+//
+//  Created by Karlis Cars on 27/06/2020.
+//  Copyright © 2020 Karlis Cars. All rights reserved.
+//
+
+import UIKit
+
+class goldenRatioViewViewController: UIViewController {
+
+    @IBOutlet weak var highLabel: UILabel!
+    @IBOutlet weak var lowLabel: UILabel!
+    
+    var x
+    var y
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+        
+        
+    }
+    @IBAction func inputNumber(_ sender: Any) {
+    }
+    @IBAction func searchButton(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
